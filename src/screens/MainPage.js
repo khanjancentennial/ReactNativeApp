131c3ba4fbfcbea50c0b15eb44295fec4a6a5c53
Bhargav <bhargav.borse4@gmail.com>
2023-12-06 07:45:52 -0500

    justify center removed

--- src/screens/MainPage.js
+++ src/screens/MainPage.js
@@ -18,7 +18,7 @@ function MainPage({ navigation, route }) {
 
         const { data } = await response.json();
         const { firstName, lastName, healthcareProvider } = data;
-
+        console.log('Fetched User Data:', data);
         setUserName(`${firstName} ${lastName}`);
         setHealthcareProviderRole(healthcareProvider);
       } catch (error) {
@@ -117,7 +117,8 @@ function MainPage({ navigation, route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    paddingTop: 20,
     alignItems: 'center',
     backgroundColor: '#EFE1E1',
     width: '100%',
